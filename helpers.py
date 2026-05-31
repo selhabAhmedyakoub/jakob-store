af479846556b6cfd4ef6bcc4646dd7d2f8265d3b
Ahmed Yakoub Selhab <selhabyaakoub55@gmail.com>
2026-05-31 16:27:57 +0200

    migrate to Flask-SQLAlchemy + PostgreSQL, fix deployment on Render

--- helpers.py
+++ helpers.py
@@ -1,28 +1,12 @@
-import csv
-import datetime
-import pytz
-import requests
-import urllib
-import uuid
-
-from flask import redirect, render_template, session
+from flask import redirect, session
 from functools import wraps
 
 
-
-
 def login_required(f):
-    """
-    Decorate routes to require login.
-
-    https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
-    """
-
+    """Decorate routes to require login."""
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
-
     return decorated_function
-
